bfe6e25e9b2ff086501fef90c31655f1688bd1a9
GP00 <2touryuu2@gmail.com>
2020-05-10 18:36:44 +0000

    favorite, unfavorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,17 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites
+  has_many :favoritelist, through: :favorites, source: :micropost
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :favoriters, through: :reverses_of_favorite, source: :user
+  
   
   def follow(other_user)
     unless self == other_user
@@ -25,6 +32,22 @@ class User < ApplicationRecord
 
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  
+  def micropost(other_user)
+    unless self == other_user
+      self.favorites.find_or_create_by(micropost_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    favorite = self.favorites.find_by(micropost_id: other_user.id)
+    favorite.destroy if favorite
+  end
+
+  def following?(other_user)
+    self.favoritelist.include?(other_user)
   end
   
   def feed_microposts
